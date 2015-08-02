bd1263dd8b61ceb7c89e5819c32e5f3dbe12dd05
Pawan <pawan.kumar1@myntra.com>
2015-08-03 00:05:48 +0530

    debounce button

--- _2/_2.ino
+++ _2/_2.ino
@@ -5,6 +5,7 @@
 int switchPin = 8;
 int ledPin = 13;
 boolean lastButton = LOW;
+boolean currentButton = LOW;
 boolean isLedOn = false;
 
 void setup() {
@@ -13,12 +14,20 @@ void setup() {
 
 }
 
-void loop() {
-  if(digitalRead(switchPin) == HIGH && lastButton == LOW){
-    isLedOn != isLedOn;
-    lastButton = HIGH;
-  }else{
-    lastButton = digitalRead(switchPin);
+boolean debounce(boolean last){
+  boolean current = digitalRead(switchPin);
+  if(current != last){
+    delay(5);
+    current = digitalRead(switchPin);
   }
+  return current;
+}
+
+void loop() {
+  currentButton = debounce(lastButton);
+  if(lastButton == LOW && currentButton == HIGH){
+    isLedOn != isLedOn;
+  }
+  lastButton = currentButton;
   digitalWrite(ledPin, isLedOn);
 }
